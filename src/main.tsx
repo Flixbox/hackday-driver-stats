b385f06c6c163feb180b5b0554c9a93e4e6050dc
Felix Tietjen <f.tietjen@clevershuttle.de>
2023-03-02 11:26:54 +0100

    feat: add progress bar stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { MantineProvider } from "@mantine/core";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider
+      theme={{
+        fontFamily: "Open Sans",
+        primaryColor: "brand",
+        colors: {
+          brand: [
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+            "#03B167",
+          ],
+        },
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
